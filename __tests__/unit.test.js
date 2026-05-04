fd420c06acae769e29b3489ec8653f6e58cd21c8
Nick Mitroff <nickmitroff@gmail.com>
2026-05-03 21:42:51 -0700

    Initial commit (contains most of parts 1 and 2 because I had to restart)

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,67 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+test('phone number test 1', () => {
+    expect(isPhoneNumber('925-822-7067')).toBe(true);
+});
+test('phone number test 2', () => {
+    expect(isPhoneNumber('+1 (925) 822-7067')).toBe(true);
+});
+test('phone number test 3', () => {
+    expect(isPhoneNumber('2319847608219734576891234765')).toBe(false);
+});
+test('phone number test 4', () => {
+    expect(isPhoneNumber('92 58 22 70 67')).toBe(false);
+});
+
+test('email test 1', () => {
+    expect(isEmail('kderr@auhsdschools.org')).toBe(true);
+});
+test('email test 2', () => {
+    expect(isEmail('nmitroff@ucsd.edu')).toBe(true);
+});
+test('email test 3', () => {
+    expect(isEmail('nmitroff@@@@@ucsd.edu')).toBe(false);
+});
+test('email test 4', () => {
+    expect(isEmail('nmitroff@')).toBe(false);
+});
+
+test('password test 1', () => {
+    expect(isStrongPassword('password')).toBe(true);
+});
+test('password test 2', () => {
+    expect(isStrongPassword('pa55woRD')).toBe(true);
+});
+test('password test 3', () => {
+    expect(isStrongPassword('oidfgsuvbhoisadufhasdjkhfoijsadfhgoaiusdfgh')).toBe(false);
+});
+test('password test 4', () => {
+    expect(isStrongPassword('4password')).toBe(false);
+});
+
+test('date test 1', () => {
+    expect(isDate('1/22/2026')).toBe(true);
+});
+test('date test 2', () => {
+    expect(isDate('55/5/5555')).toBe(true);
+});
+test('date test 3', () => {
+    expect(isDate('2026/11/22')).toBe(false);
+});
+test('date test 4', () => {
+    expect(isDate('11 22 2026')).toBe(false);
+});
+
+test('hex test 1', () => {
+    expect(isHexColor('FFF33F')).toBe(true);
+});
+test('hex test 2', () => {
+    expect(isHexColor('#FFFFAA')).toBe(true);
+});
+test('hex test 3', () => {
+    expect(isHexColor('FFFFFG')).toBe(false);
+});
+test('hextest 4', () => {
+    expect(isHexColor('1234567')).toBe(false);
+});
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,67 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+const synth = window.speechSynthesis;
+
+let voiceSelect = document.getElementById("voice-select");
+let button;
+let textarea = document.querySelector("textarea");
+let voices = synth.getVoices();
+let curVoice;
+let message;
+let smile;
+
 function init() {
-  // TODO
+  console.log(voiceSelect);
+  populateVoiceList();
+  synth.onvoiceschanged = () => {
+    populateVoiceList();
+  }
+  const explore = document.getElementById("explore");
+  button = explore.querySelector("button");
+  smile = explore.querySelector("img");
+
+  addEventListener("click", async (e) => {await onClick(e)});
+  textarea.addEventListener("change", onTextChange);
+  voiceSelect.addEventListener("change", onVoiceChange);
+}
+
+// from the SpeechSynthesis page on the MDN docs
+function populateVoiceList() {
+  voices = synth.getVoices();
+  console.log(voices.length);
+
+  for (const voice of voices) {
+    console.log(voice);
+    const option = document.createElement("option");
+    option.textContent = `${voice.name} (${voice.lang})`;
+
+    if (voice.default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    voiceSelect.appendChild(option);
+
+  }
+
+}
+
+function onTextChange(e) {
+  message = e.target.value;
+}
+
+function onVoiceChange(e) {
+  curVoice = voices[e.target.selectedIndex];
+}
+
+async function onClick(e) {
+  if (e.target == button) {
+    const utterThis = new SpeechSynthesisUtterance(message);
+    utterThis.voice = curVoice;
+    smile.src = "assets/images/smiling-open.png";
+    utterThis.addEventListener("end", (e) => {smile.src = "assets/images/smiling.png";});
+    synth.speak(utterThis);
+  }
 }
